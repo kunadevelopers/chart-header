723468f60d490f69b35a569ccd3ead225124012d
Maksym Tymchyk <maksym.tymchyk@gmail.com>
2019-07-11 22:23:06 +0300

    Move to Webpack

--- rollup.config.js
+++ rollup.config.js
@@ -6,6 +6,9 @@ import commonJs from 'rollup-plugin-commonjs';
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot';
 import { terser } from 'rollup-plugin-terser';
 import nodeResolve from 'rollup-plugin-node-resolve';
+import postcss from 'rollup-plugin-postcss';
+import autoprefixer from 'autoprefixer';
+import clean from 'postcss-clean';
 
 var environment = process.env.ENV || 'development';
 var isDevelopmentEnv = (environment === 'development');
@@ -18,18 +21,32 @@ export default {
         sourceMap: false,
         format: 'umd',
     },
+    external: [
+        'react',
+        'react-proptypes',
+    ],
     plugins: [
         replace({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
         }),
         typeScript({ tsconfig: 'tsconfig.json' }),
         nodeResolve({ jsnext: true, main: true }),
+        postcss({
+            extract: true,  // extracts to `${basename(dest)}.css`
+            plugins: [autoprefixer, clean],
+            writeDefinitions: true,
+        }),
         commonJs({
-            include: [
-                'node_modules/**',
-            ],
+            include: /node_modules/,
+            ignoreGlobal: true,
+            namedExports: {
+                'node_modules/react/index.js': getNamedExports([
+                    'react',
+                    'react-dom',
+                ]),
+            },
         }),
         sizeSnapshot(),
-        !isDevelopmentEnv && terser()
+        !isDevelopmentEnv && terser(),
     ],
 };
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const os = require('os');
+const path = require('path');
+const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const isDev = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+    mode: isDev ? 'development' : 'production',
+    devtool: isDev ? 'source-map' : false,
+
+    context: path.resolve(process.cwd(), 'src'),
+
+    entry: {
+        'kuna-chart-header': './index.tsx',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        libraryTarget: 'umd',
+        library: 'KunaChartHeader',
+        umdNamedDefine: true,
+    },
+    resolve: {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+    },
+    plugins: [],
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+                exclude: /node_modules/,
+                query: {
+                    declaration: false,
+                },
+            }],
+    },
+
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                sourceMap: isDev,
+                uglifyOptions: {
+                    output: {
+                        comments: false,
+                    },
+                },
+                parallel: os.cpus().length,
+            }),
+
+            new OptimizeCSSAssetsPlugin({
+                cssProcessorOptions: { discardComments: { removeAll: true } },
+            }),
+        ],
+    },
+};
