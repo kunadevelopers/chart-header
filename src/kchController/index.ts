8b059ee52f447af9a19152292951ebff3294f255
Tymchyk Maksym <reilag69@gmail.com>
2019-07-20 19:21:10 +0300

    Update Charts

--- src/kchController/index.ts
+++ src/kchController/index.ts
@@ -1,6 +1,8 @@
-import { observable, set, action } from 'mobx';
+import { Pusher } from 'pusher-js';
+import { observable, set, action, runInAction } from 'mobx';
 
 export type KCHOption = {
+    symbol?: string;
     baseAsset?: string;
     quoteAsset?: string;
     change24h?: number;
@@ -13,9 +15,16 @@ export type KCHOption = {
 
     pricePrecision?: number;
     volumePrecision?: number;
+
+    volume24h?: number;
 };
 
 export default class KCHController implements mobx.IKCHSStore {
+    protected pusher?: Pusher;
+
+    @observable
+    public symbol?: string;
+
     @observable
     public baseAsset?: string;
 
@@ -35,6 +44,12 @@ export default class KCHController implements mobx.IKCHSStore {
     public low?: number;
 
     @observable
+    public change24h?: number;
+
+    @observable
+    public volume24h?: number;
+
+    @observable
     public pricePrecision: number = 2;
 
     @observable
@@ -42,6 +57,11 @@ export default class KCHController implements mobx.IKCHSStore {
 
     public constructor(option: KCHOption) {
         set(this, option);
+
+        if ('pusher' in window) {
+            this.pusher = (window as any).pusher;
+            this.startTracking();
+        }
     }
 
     @action
@@ -59,5 +79,45 @@ export default class KCHController implements mobx.IKCHSStore {
         }
 
         set(this, option);
+    }
+
+    @action
+    protected handleTickers(tickers: Record<string, any>) {
+        if (!this.symbol) {
+            return;
+        }
+
+        const ticker = tickers[this.symbol];
+        const lastPrice = +ticker.last;
+        const openPrice = +ticker.open;
+
+        runInAction(() => {
+            this.volume24h = +ticker.volume;
+
+            this.low = +ticker.low;
+            this.high = +ticker.high;
+
+            this.lastPrice = lastPrice;
+
+            if (openPrice && openPrice > 0) {
+                this.change24h = (lastPrice - openPrice) / openPrice;
+            }
+        });
+    }
+
+    protected startTracking() {
+        if (!this.pusher) {
+            return;
+        }
+
+        const channelName = ['market', 'global'].join('-');
+        const event = 'tickers';
+
+        let globalChannel = this.pusher.channel(channelName);
+        if (!globalChannel) {
+            globalChannel = this.pusher.subscribe(channelName);
+        }
+
+        globalChannel.bind(event, this.handleTickers.bind(this));
     }
 }
